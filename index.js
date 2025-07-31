97c91759701362753fa0580f0cf7ed3247c7e48e
lorranfelix <104112375+lorranfelix@users.noreply.github.com>
2025-07-30 22:32:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const app = express();
-app.use(cors());
-app.use(express.json());
 
 // Importando a conexão com o banco de dados
 const conn = require('./db/conn');  
 
 conn(); // Iniciando a conexão com o MongoDB
 
-//routes
-const routes = require('./routes/router');
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use('/api', routes);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch(err => console.log("Erro MongoDB:", err));
 
 
-
-
-
-app.listen(3000, function() {
-    console.log('Servidor rodando na porta 3000');
+// Rota GET
+app.get('/', (req, res) => {
+  res.json({ mensagem: "API Unreal está online 1.1" });
 });
+
+
 
 // Porta para o Heroku
 const PORT = process.env.PORT || 3000;
